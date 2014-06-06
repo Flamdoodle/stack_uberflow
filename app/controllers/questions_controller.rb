9d7623c5af390d91ae279df6f3d61ec282db4d1b
Pei Liu & Sahan Pitigala <pair+girb0t.spitigala@devbootcamp.com>
2014-06-06 12:19:56 -0400

    Set up routes, setup QuestionController skeleton

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,28 @@
+class QuestionController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def upvote
+  end
+
+  def downvote
+  end
+end
