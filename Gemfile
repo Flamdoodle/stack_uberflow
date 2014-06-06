dfb7bd5a12ed1e4ba5ddb5abb3b10259246fb81f
Connor Harwood <harwood.connor@gmail.com>
2014-06-06 11:25:07 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.18'
+
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'faker'
+  gem 'factory_girl_rails'
+  gem 'capybara'
+  gem 'shoulda-matchers'
+  gem 'selenium-webdriver', '~>2.38.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StackUberflow::Application.config.secret_token = 'a4774e1a46bbc44c09ccf77bb3534eb6213328019ab0f82ea4cfdc74fed329b1859f34b050fd12726041d0b72d78c309c6fd6bf15e30854bd528fb7b536770ec'
