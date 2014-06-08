32ca813d1ac6d01269097bf905b5fe687a562e29
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-08 10:52:20 -0400

    two tests created in models of question slice

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,3 +1,18 @@
 require 'spec_helper'
+require_relative '../spec_helper.rb'
 
+describe Question do 
+	it "should create a title and body in the question" do
+		question = Question.new(
+			title: 'What is rails?'
+			body: 'Just curious',
+			)
+	end
 
+	it "should create a question even without a title" do
+			question = Question.new(
+				title: nil,
+				body: 'Just curious',
+				)
+		end
+end
