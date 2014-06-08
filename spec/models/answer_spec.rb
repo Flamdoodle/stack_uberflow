3ec2fdd0353c532e8fb05a14d73eef0fe8192eca
Stephen Roth <stephenroth.sr@gmail.com>
2014-06-08 19:51:54 -0400

    added some model tests to ensure validation for lawrence and nirav

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,10 +1,22 @@
 require_relative '../spec_helper'
 
 describe Answer do
-  it {should have_many(:votes)}
-  it {should belong_to(:answerer)}
-  it {should_not belong_to(:asker)}
-  it {should_not belong_to(:user)}
-  it {should belong_to(:question)}
+  context "associations" do
+    it {should have_many(:votes)}
+    it {should belong_to(:answerer)}
+    it {should_not belong_to(:asker)}
+    it {should_not belong_to(:user)}
+    it {should belong_to(:question)}
+  end
+
+  context "creating an answer" do
+    it "creates an answer when there is a body" do
+      answer = Answer.new(
+        body: 'Just curious'
+        )
+      expect(answer).to be_valid
+    end
+  end
 end
+
 
