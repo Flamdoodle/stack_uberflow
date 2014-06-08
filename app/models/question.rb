78788d193db5b0f34e009852cf0cb4752f18e03b
Nirav Chheda <niravchheda@gmail.com>
2014-06-08 14:36:06 -0400

    index deleted

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,9 @@
 class Question < ActiveRecord::Base
-  attr_accessible :title, :body, :answer, :user 
+  attr_accessible :title, :body, :answer, :user, :asker
   belongs_to :asker, class_name: "User"
   has_many :votes, as: :voteable
   has_many :answers
 
-  validates :title, length: { minimum: 2 }, presence: true 
-  validates :body, presence: true 
+  validates :title, length: { minimum: 2 }, presence: true
+  validates :body, presence: true
 end
