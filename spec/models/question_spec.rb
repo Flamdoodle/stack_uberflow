927e6e93fac51d95057c9f87d329f021ce628706
Payam Pakmanesh <p.pakmanesh@gmail.com>
2014-06-08 10:05:39 -0400

    question folders and files added/modified, ready to add code for testing

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,10 +1,3 @@
-require 'rails_helper'
+require 'spec_helper'
 
 
-describe Question do
-  it {should have_many(:votes)}
-  it {should belong_to(:asker)}
-  it {should_not belong_to(:answerer)}
-  it {should_not belong_to(:user)}
-  it {should have_many(:answers)}
-end
