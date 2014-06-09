19509aa21c5156041d5c62ee2ac3600346f54da0
Kenneth Bruce Uy <missingno15@gmail.com>
2014-06-09 09:06:31 -0400

    Fixed typo

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ActiveRecord::Base
-  attr_accessible :vote_value, :user, :voteable_type, :voteables
+  attr_accessible :vote_value, :user, :voteable_type, :voteable
 
   belongs_to :voteable, polymorphic: true
 
